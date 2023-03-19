5379c949609811dbe362212dbc1b8a13351d4b13
winglechen <winglechen@gmail.com>
2023-03-19 21:07:35 +0800

    feature: add config templates and application module

---
+++ pywolf/application/context.py
@@ -0,0 +1,21 @@
+
+class ApplicationContext(dict):
+    def __init__():
+        pass
+
+    def setRootPath(self, path):
+        self.rootPath = path
+        self.bootstrap()
+
+    def getRootPath(self):
+        return self.rootPath
+
+    def bootstrap(self):
+        pass
+
+    def get(self, key):
+        pass
+
+
+app=appContext=applicationContext = new ApplicationContext()
+
