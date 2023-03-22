e9c4cb41fcf3a85b6c6656907445c5469db52443
winglechen <winglechen@gmail.com>
2023-03-23 00:23:03 +0800

    feature: impl db.Executor.insert

--- pywolf/db/executor.py
+++ pywolf/db/executor.py
@@ -1,27 +1,34 @@
-
+from databases import Database
 from pywolf.db.result import Result
 from pywolf.db import sqlutils
 
 class Executor(object):
-    def __init__(self, conn) -> None:
+    def __init__(self, conn: Database) -> None:
         self.conn = conn
 
-    async def execute(self, sql: str) -> Result:
+    async def execute(self, sql: str, values=None) -> Result:
         if not sql:
             raise SystemError('invalid sql: ' + sql)
         
         if sqlutils.is_select(sql):
             return await self.select(sql)
 
-
         resp = await self.conn.execute(sql)
         if not resp:
-            return Result.from_affected_rows(resp)
+            return Result.from_affected_rows(0)
 
-        return Result.from_affected_rows(0)
+        if sqlutils.is_insert(sql):
+            return Result.from_last_id(resp)
+
+        return Result.from_affected_rows(resp)
     
     async def insert(self, table:str, values):
-        return None
+        sql = sqlutils.build_insert_sql(table, values)
+        if isinstance(values, dict):
+            return await self.execute(sql, values)
+        
+        await self.conn.execute_many(sql, values)
+        return Result.from_affected_rows(len(values))
     
     async def select(self, sql: str):
         return None
