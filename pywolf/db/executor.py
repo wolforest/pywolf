b4812bf7af27df944913e4030046248e692fe0a7
winglechen <winglechen@gmail.com>
2023-03-23 00:31:53 +0800

    bugfix: fix db.Executor return data type bug

--- pywolf/db/executor.py
+++ pywolf/db/executor.py
@@ -3,7 +3,7 @@ from pywolf.db.result import Result
 from pywolf.db import sqlutils
 
 class Executor(object):
-    def __init__(self, conn: Database) -> None:
+    def __init__(self, conn: Database):
         self.conn = conn
 
     async def execute(self, sql: str, values=None) -> Result:
@@ -22,19 +22,13 @@ class Executor(object):
 
         return Result.from_affected_rows(resp)
     
-    async def insert(self, table:str, values):
+    async def insert(self, table:str, values) -> Result:
         sql = sqlutils.build_insert_sql(table, values)
         if isinstance(values, dict):
             return await self.execute(sql, values)
         
         await self.conn.execute_many(sql, values)
         return Result.from_affected_rows(len(values))
-    
+
     async def select(self, sql: str):
         return None
-    
-
-    
-   
-    
-
