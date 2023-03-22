b05bdc2e1d8bccd23413fedceaac2dfd211ecf37
winglechen <winglechen@gmail.com>
2023-03-22 23:57:41 +0800

    feature: add join_columns join_values to sqlutils

--- pywolf/db/sqlutils.py
+++ pywolf/db/sqlutils.py
@@ -11,3 +11,33 @@ def is_delete(sql: str) -> bool:
 
 def is_insert(sql: str) -> bool:
     return strutils.starts_with(sql, "insert")
+
+def get_first_insert_row(values):
+    if not values:
+        raise SyntaxError('invalid insert values')
+
+    if isinstance(values, dict):
+        return values
+    elif isinstance(values, list) and len(values) > 0 and isinstance(values[0], dict):
+        return values[0]
+    else :
+        raise SyntaxError('invalid insert values')
+
+def build_insert_sql(table: str, values):
+    if not table or not values:
+        raise SyntaxError('invalid insert syntax')
+    
+    first_row = get_first_insert_row(values)
+    columns = first_row.keys()
+
+    sql = 'INSERT INTO '. join(table, '(', ') values (', ')')
+
+
+def join_columns(columns: list) -> str:
+    return '`' + '`, `'.join(columns) + '`'
+    
+def join_values(columns: list) -> str:
+    return ':' + ', :'.join(columns)
+
+
+ 
