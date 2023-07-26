82a788f7f7c33ddad6624ea16e09d19896efaeb7
winglechen <winglechen@gmail.com>
2023-07-26 20:58:55 +0800

    feature: add urlparser

---
+++ pywolf/net/urlparser.py
@@ -0,0 +1,10 @@
+from urllib.parse import urlparse
+from urllib.parse import ParseResult
+
+class Url(ParseResult):
+    def __init__(self):
+        super().__init()
+
+    def __init__(self, result: ParseResult):
+        pass
+
